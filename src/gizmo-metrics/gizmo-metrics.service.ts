a1039aa99a4d5e16ab63e32469d69a465cb971c9
Crow <yangchen.xiyou@gmail.com>
2024-01-25 20:22:18 +0800

    feat: 修改字段类型

--- src/gizmo-metrics/gizmo-metrics.service.ts
+++ src/gizmo-metrics/gizmo-metrics.service.ts
@@ -91,15 +91,20 @@ export class GizmoMetricsService {
     );
   }
 
-  getNumConversationsStr(str: string): string {
+  getNumConversationsStr(str: string): number {
     if (str) {
       let result = str.replace('+', '');
       if (result.indexOf('K') > -1) {
         result = result.replace('K', '');
-        return `${+result * 1000}`;
+        return +result * 1000;
       }
 
-      return result;
+      if (result.indexOf('M') > -1) {
+        result = result.replace('M', '');
+        return +result * 1000 * 1000;
+      }
+
+      return +result;
     }
 
     return undefined;
