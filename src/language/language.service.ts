b7a26445f8a9c83966c3414378533be8a62ef2d8
taibai <yangchen.xiyou@gmial.com>
2024-02-05 02:38:14 +0800

    feat: 测试语言问题

--- src/language/language.service.ts
+++ src/language/language.service.ts
@@ -23,6 +23,7 @@ export class LanguageService {
 
   async create(createLanguageDto: CreateLanguageDto) {
     const old = await this.findOne(createLanguageDto.id);
+    this.logger.info('%s, %s', old, createLanguageDto.id);
     if (!old) {
       this.prismaService.language.create({
         data: createLanguageDto,
